ce0ee136519ed9b2b82099bf6de90ef8d2d6115e
woojin.kim <wjikim@naver.com>
2026-02-18 10:47:49 +0900

    Remove build folder

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_PowerStageAnalyzer_FILE E:/workspace/Qt/DCDCPowerAnalyzer/build/PowerStageAnalyzer.exe)
-set(__QT_DEPLOY_TARGET_PowerStageAnalyzer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PowerStageAnalyzer_RUNTIME_DLLS E:/Qt/6.10.1/mingw_64/bin/Qt6Charts.dll;E:/Qt/6.10.1/mingw_64/bin/Qt6OpenGLWidgets.dll;E:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;E:/Qt/6.10.1/mingw_64/bin/Qt6OpenGL.dll;E:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;E:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
